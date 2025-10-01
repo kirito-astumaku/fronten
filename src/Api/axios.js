2a9721f5fb12c2a769087fbac21c597608264447
Manuel Francisco Moreno Durán <158637222+kirito-astumaku@users.noreply.github.com>
2025-10-01 17:04:28 -0600

    adsga

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-  baseURL: 'https://basededatos-production-66c6.up.railway.app',
+  baseURL: 'https://basededatos-production-66c6.up.railway.app/api',
   withCredentials: true,
 });
 
