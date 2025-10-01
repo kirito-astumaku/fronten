157f85ede171be82d4257160b88fc6782b1a77f9
Manuel Francisco Moreno Durán <158637222+kirito-astumaku@users.noreply.github.com>
2025-10-01 16:10:19 -0600

    abcd

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom"
 import Rutas from "./Routers/Rutas"
 import Navbar from "./components/Navbar";
 import { AuthProvider } from "./context/AuthContext";
+import './app.css';
 
 function App() {
 
